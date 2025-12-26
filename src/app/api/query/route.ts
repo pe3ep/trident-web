670520b099c227e06c5df42575c44b379e7d3b2a
Andrew <legendarysuwe@gmail.com>
2025-12-27 00:12:01 +0300

    ✨ New doc pages + query api

---
+++ src/app/api/query/route.ts
@@ -0,0 +1,43 @@
+export async function POST(request: Request) {
+  let user = request.headers.get('x-mc-uuid')
+  if (!user) {
+    return Response.json({ success: false, error: 'x-mc-uuid is empty' }, { status: 400 })
+  }
+
+  const getUUId = await fetch(`https://api.minecraftservices.com/minecraft/profile/lookup/${user}`, {
+    cache: 'force-cache',
+  })
+    .then((res) => res.json())
+    .catch((e) => console.log(e))
+
+  let uuid = getUUId?.id || null
+  if (uuid == null) {
+    return Response.json({ success: false, error: 'Invalid UUID in x-mc-uuid' }, { status: 400 })
+  }
+
+  // TODO: Add ratelimiting based on uuid/ip/whatever
+
+  // MCCI API - Headers
+  const headers: HeadersInit = new Headers()
+  headers.set('Content-Type', 'application/json')
+  headers.set('User-Agent', 'Trident Web')
+  headers.set('X-API-Key', process.env.NOX_TOKEN!!)
+
+  const body = await request.json()
+
+  let mcci = await fetch('https://api.mccisland.net/graphql', {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .catch((e) => {
+      console.log(e)
+    })
+
+  if (!mcci) {
+    return Response.json({ success: false, error: "Error occured when querying MCCI's API" }, { status: 500 })
+  }
+  mcci.success = true
+  return Response.json(mcci)
+}
--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,10 +1,15 @@
 import TridentLogo from '@/components/icons/trident'
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
+import { ArrowLeft } from 'lucide-react'
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: <TridentLogo className="fill-foreground size-6" />,
+      title: (
+        <>
+          <TridentLogo className="fill-foreground size-6" />
+        </>
+      ),
       enabled: false,
     },
   }
